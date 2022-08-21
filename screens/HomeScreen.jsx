98b02dd90b04de35d7be8913350aa0ad3151bd27
Marcelo_Bettini <marcelobettini@yahoo.com.ar>
2022-08-21 20:46:28 -0300

    Navigation removed. Added sports category

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,13 +1,12 @@
-import { Image, TouchableNativeFeedback, View, Text, FlatList } from 'react-native';
+import { TouchableNativeFeedback, View, Text, FlatList } from 'react-native';
 import { useState } from 'react';
 import ItemSeparator from '../components/ItemSeparator';
 import NewsItem from '../components/NewsItem';
 import { styles } from '../styles/styles';
 import useFetch from '../helpers/useFetch';
 
-export default function HomeScreen({ navigation }) {  
+export default function HomeScreen() {
   const [params, setParams] = useState('?country=us')
-  
   const [data, error, isLoading] = useFetch(params)
 
   return isLoading ?
@@ -24,26 +23,22 @@ export default function HomeScreen({ navigation }) {
       (<View style={[styles.pad, styles.container]}>
 
         <FlatList
-          data={data.articles}  
+          data={data.articles}
           showsVerticalScrollIndicator={false}
           ListHeaderComponent={
             <Text style={[styles.heading, styles.mb3]}>latest news</Text>
           }
           keyExtractor={item => item.publishedAt}
           renderItem={({ item }) => <NewsItem item={item} />}
-          ItemSeparatorComponent={()=><ItemSeparator/>}
+          ItemSeparatorComponent={() => <ItemSeparator />}
         />
 
         <View style={{ width: "100%", flexDirection: 'row', justifyContent: "space-around" }}>
-          <TouchableNativeFeedback onPress={() => navigation.navigate('Profile', { name: "Lupe" })}>
-            <Image
-              style={styles.tabIcon}
-              source={require('../assets/profile.png')} />
+          <TouchableNativeFeedback>
+            <Text>economy</Text>
           </TouchableNativeFeedback>
-          <TouchableNativeFeedback onPress={() => navigation.navigate('Config', setParams={setParams})}>
-            <Image
-              style={styles.tabIcon}
-              source={require('../assets/config.png')} />
+          <TouchableNativeFeedback onPress={() => setParams('?category=sports')}>
+            <Text>sports</Text>
           </TouchableNativeFeedback>
         </View>
       </View>
